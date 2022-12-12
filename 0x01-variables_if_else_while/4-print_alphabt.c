e3168cb817237f19398864976fbed35cc62a17bd
habjem <habjem2022@gmail.com>
2022-12-12 07:10:08 -0800

    dec12 8th commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,10 +12,11 @@ int main(void)
 	char ch;
 
 	for (ch = 'a' ; ch <= 'e' ; ch++)
-	for (ch = 'f' ; ch <= 'q' ; ch++)
-	for (ch = 'r' ; ch <= 'z' ; ch++)
 	{
-		putchar(ch);
+	if (ch == 'e' || ch 'q')
+	{continue; }
+
+	putchar(ch);
 	}
 	putchar('\n');
 	return (0);
