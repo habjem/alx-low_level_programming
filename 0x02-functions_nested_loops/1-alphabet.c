e86429623bfdbe7c22988d80f211a0ccbbe36ed0
habjem <habjem2022@gmail.com>
2022-12-13 07:43:01 -0800

    dec13 10th commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	int x;
+
+	print_alphabet(x);
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int print_alphabet(int x)
+	{
+	for (x = 'a' ; x <= 'z' ; x++)
+		{
+		putchar (x);
+		}
+	putchar('\n');
+	return (x);
+	}
